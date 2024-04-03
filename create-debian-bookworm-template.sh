c9dbc453e16ffe9b041c3ee6a945b46c24908fd5
bradleyb <bradleyb73@gmail.com>
2024-04-03 21:08:23 +0100

    added debian

---
+++ create-debian-bookworm-template.sh
@@ -0,0 +1,34 @@
+imageURL=https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-genericcloud-amd64.qcow2
+imageName="debian-12-genericcloud-amd64.qcow2"
+volumeName="local-ssd"
+virtualMachineId="9003"
+templateName="debain-12-cloud-tmpl"
+tmp_cores="2"
+tmp_memory="2048"
+rootPasswd="D9dUFf7pxNnuLGa5smI8"
+cpuTypeRequired="host"
+userName="sso-user"
+userPassword="L1ght5p33d"
+sshkeys_pub="/root/.ssh/id_ed25519-butlerlab-ssh.pub"
+
+
+apt update
+apt install libguestfs-tools -y
+rm *.img
+wget -O $imageName $imageURL
+qm destroy $virtualMachineId
+virt-customize -a $imageName --install qemu-guest-agent
+virt-customize -a $imageName --root-password password:$rootPasswd
+qm create $virtualMachineId --name $templateName --memory $tmp_memory --cores $tmp_cores --net0 virtio,bridge=vmbr10
+qm importdisk $virtualMachineId $imageName $volumeName
+qm set $virtualMachineId --scsihw virtio-scsi-pci --scsi0 $volumeName:vm-$virtualMachineId-disk-0
+qm set $virtualMachineId --boot c --bootdisk scsi0
+qm set $virtualMachineId --ide2 $volumeName:cloudinit
+qm set $virtualMachineId --serial0 socket --vga serial0
+qm set $virtualMachineId --ipconfig0 ip=dhcp
+qm set $virtualMachineId --cpu cputype=$cpuTypeRequired
+qm set $virtualMachineId --agent 1
+qm set $virtualMachineId --sshkeys $sshkeys_pub
+qm set $virtualMachineId --ciuser $userName
+qm set $virtualMachineId --cipassword $userPassword
+qm template $virtualMachineId
